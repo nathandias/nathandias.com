de61b0add7d6b43665565e1f2adda4fd91828683
Nathan Dias <nathan.dias@gmail.com>
2021-04-25 16:33:32 -0700

    remove/comment out heroku specific settings

--- nathandias_com/settings.py
+++ nathandias_com/settings.py
@@ -165,10 +165,10 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# # Heroku: Update database configuration from $DATABASE_URL.
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
