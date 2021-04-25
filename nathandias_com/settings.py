8de879a13720853bd708fe1083937012f9278452
Nathan Dias <nathan.dias@gmail.com>
2021-04-25 15:33:59 -0700

    remove unused code

--- nathandias_com/settings.py
+++ nathandias_com/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import json
-from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
